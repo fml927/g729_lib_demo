a90c2118acc5229bc0de83e985e0c215b9f3fe74
fengmingliang <mingliang.feng@zerouav.com>
2016-01-06 15:52:11 +0800

    refresh g729

---
+++ g729_lib/autogen.sh
@@ -0,0 +1,27 @@
+echo libtoolize
+if [ `(uname -s) 2>/dev/null` == 'Darwin' ]
+then
+	glibtoolize --copy --force
+else
+	libtoolize --copy --force
+fi
+
+#FML
+#echo autoscan
+#autoscan
+#mv configure.scan configure.in
+
+echo "aclocal"
+aclocal
+
+echo "autoheader"
+autoheader
+
+echo "create NEWS, README, AUTHORS and ChangeLog"
+touch NEWS README AUTHORS ChangeLog
+
+echo "autoreconf"
+autoreconf -i
+
+echo "automake"
+automake -a
